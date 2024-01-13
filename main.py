c5d66eaf6f68c972769e190aa518fe6915ce964a
ODUME VINCENT TUOYO <odumevincent19@gmail.com>
2024-01-13 16:51:58 +0100

    restructured code

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 
 
+app = FastAPI(title="G-Maps Gastronomy Data")
 
 chrome_options = Options()
-chrome_options.add_argument("--headless=new")
+# chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 # chrome_options.add_argument("--disable-blink-features=AutomationControlled")
@@ -29,21 +30,50 @@ driver = webdriver.Chrome(options=chrome_options)
 
 gastronomy_data = []
 
-def scrapePage(url):
-    wait = WebDriverWait(driver, 10)
-    print("Fetching url...")
-    driver.get(url)
-    print("URL fetched")
-    sleep(5)
 
+@app.get("/")
+async def home():
+    """
+    Click on "Try it out" button at the top right of every route, provide needed details and click on "Execute" to run the code
+    """
+    return {
+        1: "Steps to scrape gastronomy data",
+        2: "Query your browser manually for city you want gastronomy data on e.g restaurants in Germany",
+        3: "Click on 'more places' to load list of restaurants in google maps",
+        4: "Copy url of page frop step 2.",
+        5: "Sampled_url = https://www.google.com/search?client=firefox-b-d&sca_esv=594603375&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u5!2m2!5m1!1sgcid_3german_1restaurant!1m4!1u5!2m2!5m1!1sgcid_3indian_1restaurant!1m4!1u2!2m2!2m1!1e1!1m4!1u1!2m2!1m1!1e1!1m4!1u1!2m2!1m1!1e2!2m1!1e2!2m1!1e5!2m1!1e1!2m1!1e3!3sIAEqAkRF,lf:1,lf_ui:9&tbm=lcl&sxsrf=AM9HkKnqdXFj_FVNzgEjVYXigqgBUOtXnw:1703948168668&q=restaurants%20in%20germany&rflfq=1&num=10&sa=X&ved=2ahUKEwinmbzKtbeDAxWfS0EAHQLRDoEQjGp6BAgXEAE&biw=1525&bih=760&dpr=0.9&rlst=f#rlfi=hd:;si:;mv:[[52.809863099999994,14.150356],[47.8622162,6.5175032]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u5!2m2!5m1!1sgcid_3german_1restaurant!1m4!1u5!2m2!5m1!1sgcid_3indian_1restaurant!1m4!1u2!2m2!2m1!1e1!1m4!1u1!2m2!1m1!1e1!1m4!1u1!2m2!1m1!1e2!2m1!1e2!2m1!1e5!2m1!1e1!2m1!1e3!3sIAEqAkRF,lf:1,lf_ui:9",
+        6: "Paste url in url-field of scrape-Page route",
+        7: "Click on Execute to scrape data",
+        8: "The scraping would take some time before it completes so as to avoid Googles suspicion and captcha test",
+        9: "Append /docs to this page's url to continue"
+    }
+
+@app.get("/data-via-page-url")
+async def scrape_page(url: str):
+    if not url:
+        raise HTTPException(status_code=404, detail="Input a url to scrape data")
+
+    wait = WebDriverWait(driver, 10)
+    try:
+        print("Fetching url...")
+        driver.get(url)
+        print("URL fetched")
+        sleep(5)
+    except Exception as e:
+        return {
+            "message": "Connectivity error, check your internet connection",
+            "Error": e
+        }
+    
+    restaurants_links = None
     for i in range(5):
         sleep(2)
-        # retry mechanism for error sake
+        # retry mechanism to fetch restaurants links
         for attempt in range(3):
             try:
-                restaurants_links = wait.until(EC.visibility_of_all_elements_located(locator=(By.CSS_SELECTOR, "a.vwVdIc")))
+                restaurants_links = wait.until(EC.presence_of_all_elements_located(locator=(By.CSS_SELECTOR, "a.vwVdIc")))
                 # driver.find_elements(By.CSS_SELECTOR, "a.vwVdIc")
-                sleep(10)
+                # sleep(10)
                 print(f"Scraping {i+1} of {len(restaurants_links)} restaurants")
                 restaurant = restaurants_links[i]
                 break
@@ -51,11 +81,24 @@ def scrapePage(url):
                 print(f"Attempt {attempt+1} to get restaurants failed: {e}")
                 if attempt < 2:
                     sleep(3)
+                else:
+                    return {
+                        "Error": e,
+                        "message": "Error fetching lists of resaurants from provided url",
+                        "fix": "Ensure you have a strong internet connection"
+                    }
 
-        # click and wait till the clicked restaurant main page loads
+        # get restaurant's nationality before clicking restaurant
+        # to do
+        # nationality_parent_element = restaurant.find_element(By.CSS_SELECTOR, "div.rllt_details:nth-child(2)")
+        # nationality = nationality_parent_element.find_elements(By.XPATH, "./*")
+        # print(nationality[-1])
+        
+        # click and wait till the clicked restaurant page loads
         restaurant.click()
         wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.kp-header")))
 
+        # parse popped-up page to beautiful soup
         page = soup(driver.page_source, "html.parser")
 
         try:
@@ -82,22 +125,26 @@ def scrapePage(url):
                 address = "Nil"
 
             # restaurant nationality
-            for attempt in range(3):
+            try:
+                nationality = page.select("span.YhemCb")[1].text
+                print(f"Nationality: {nationality}")
+            except IndexError:
+                print("Index Error encountered...")
+                nationality = driver.find_element(By.CSS_SELECTOR, "div.zloOqf span.YhemCb:nth-child(2)").text
+                print(f"Nationality: {nationality}")
+            except NoSuchElementException:
+                print("First No such element encountered")
                 try:
-                    nationality = page.select("span.YhemCb")[1].text
+                    nationality = driver.find_element(By.CSS_SELECTOR, "div.zloOqf span.YhemCb").text
                     print(f"Nationality: {nationality}")
-                    break
-                except IndexError:
-                    nationality = driver.find_element(By.CSS_SELECTOR, "div.zloOqf span.YhemCb:nth-child(2)").text
-                    print(f"Nationality: {nationality}")
-                    break
                 except NoSuchElementException:
-                    print("No such element")
+                    print(f"Nationality of {name} is not provided on their page")
                     nationality = "Nil"
-                except Exception as e:
-                    print(f"Attempt {attempt+1} failed: {e}")
-                    if attempt < 2:
-                        sleep(3)
+                    print(f"Nationality: {nationality}")
+            except Exception as e:
+                print(f"{name}'s nationality not found.\nError: {e}")
+                nationality = "Nil"
+                print(f"Nationality: {nationality}")
 
             # restaurant telephone number
             for attempt in range(3):
@@ -129,42 +176,47 @@ def scrapePage(url):
                     website_url = "Nil"
                 except Exception as e:
                     print(f"Attempt {attempt+1} failed: {e}")
+                    website_url = "Nil"
                     if attempt < 2:
                         sleep(3)
 
             # restaurant instagram link
-            for attempt in range(3):
-                try:
-                    insta_link_element = driver.find_element(By.XPATH, "//g-link/a[starts-with(@href, 'https://www.instagram.com/')]")
-                    insta_link = insta_link_element.get_attribute("href")
+            try:
+                insta_link_element = driver.find_element(By.XPATH, "//g-link/a[starts-with(@href, 'https://www.instagram.com/')]")
+                insta_link = insta_link_element.get_attribute("href")
+                print(f"Instagram Link: {insta_link}")
+            except NoSuchElementException:
+                insta_link = page.select_one("g-link a[href^='https://www.instagram.com/']")
+                if insta_link:
+                    insta_link = insta_link["href"]
                     print(f"Instagram Link: {insta_link}")
-                    break
-                except NoSuchElementException:
-                    insta_link = page.select_one("g-link a[href^='https://www.instagram.com/']")["href"]
-                    print(f"Instagram Link: {insta_link}")
-                    break
-                except Exception as e:
-                    print(f"Attempt {attempt+1} to get IG link failed: {e}")
+                else:
                     insta_link = "Nil"
-                    if attempt < 2:
-                        sleep(3)
+                    print(f"No instagram link on {name}'s page")
+                    print(f"Instagram Link: {insta_link}")
+            except Exception as e:
+                insta_link = "Nil"
+                print(f"Error finding instagram link: {e}")
+                print(f"Instagram Link: {insta_link}")
 
             # restaurant facebook link
-            for attempt in range(3):
-                try:
-                    facebook_link_element = driver.find_element(By.XPATH, "//g-link/a[starts-with(@href, 'https://www.facebook.com/')]")
-                    facebook_link = facebook_link_element.get_attribute("href")
+            try:
+                facebook_link_element = driver.find_element(By.XPATH, "//g-link/a[starts-with(@href, 'https://www.facebook.com/')]")
+                facebook_link = facebook_link_element.get_attribute("href")
+                print(f"Facebook Link: {facebook_link}")
+            except NoSuchElementException:
+                facebook_link_element = page.select_one("g-link a[href^='https://www.facebook.com/']")
+                if facebook_link_element:
+                    facebook_link = facebook_link_element["href"]
                     print(f"Facebook Link: {facebook_link}")
-                    break
-                except NoSuchElementException:
-                    facebook_link = page.select("g-link a[href^='https://www.facebook.com/']")["href"]
-                    print(f"Facebook Link: {facebook_link}")
-                    break
-                except Exception as e:
-                    print(f"Attempt {attempt+1} to get FB link failed: {e}")
+                else:
                     facebook_link = "Nil"
-                    if attempt < 2:
-                        sleep(3)
+                    print(f"No facebook link on {name}'s page")
+                    print(f"Facebook Link: {facebook_link}")
+            except Exception as e:
+                facebook_link = "Nil"
+                print(f"Error finding facebook link: {e}")
+                print(f"Facebook Link: {facebook_link}")
 
             # restaurant service options
             try:
@@ -186,64 +238,28 @@ def scrapePage(url):
                 "Telephone_Number": telephone_number,
                 "Website_url": website_url,
                 "Instagram_link": insta_link,
-                "Facebook": facebook_link,
+                "Facebook_link": facebook_link,
                 "Service_options": service_options
             }
             gastronomy_data.append(data)
             print(data)
+            print(f"Done with {i+1}. {name} \n")
 
             # go back to url main page
             driver.back()
             sleep(5)
         except Exception as e:
             print(f"Exception Error: {e}")
-
+        
     driver.quit()
     return gastronomy_data
 
-# scrapePage(mainz)
-
 def gastronomy_data_csv():
-    header = ["S/N", "Name", "Address", "Nationality", "Telephone_Number", "Website_url", "Instagram_link", "Facebook", "Service_options"]
+    header = ["S/N", "Name", "Address", "Nationality", "Telephone_Number", "Website_url", "Instagram_link", "Facebook_link", "Service_options"]
     yield ",".join(header) + "\n"
 
     for values in gastronomy_data:
         yield ",".join(values[key] for key in header) + "\n"
-
-
-app = FastAPI(title="G-Maps Gastronomy Data")
-
-@app.get("/")
-async def home():
-    """
-    Click on "Try it out" button at the top right of every route, provide needed details and click on "Execute" to run the code
-    """
-    return {
-        1: "Steps to scrape gastronomy data",
-        2: "Query your browser manually for city you want gastronomy data on e.g restaurants in Germany",
-        3: "Click on 'more places' to load list of restaurants in google maps",
-        4: "Copy url of page frop step 2.",
-        5: "Sampled_url = https://www.google.com/search?client=firefox-b-d&sca_esv=594603375&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u5!2m2!5m1!1sgcid_3german_1restaurant!1m4!1u5!2m2!5m1!1sgcid_3indian_1restaurant!1m4!1u2!2m2!2m1!1e1!1m4!1u1!2m2!1m1!1e1!1m4!1u1!2m2!1m1!1e2!2m1!1e2!2m1!1e5!2m1!1e1!2m1!1e3!3sIAEqAkRF,lf:1,lf_ui:9&tbm=lcl&sxsrf=AM9HkKnqdXFj_FVNzgEjVYXigqgBUOtXnw:1703948168668&q=restaurants%20in%20germany&rflfq=1&num=10&sa=X&ved=2ahUKEwinmbzKtbeDAxWfS0EAHQLRDoEQjGp6BAgXEAE&biw=1525&bih=760&dpr=0.9&rlst=f#rlfi=hd:;si:;mv:[[52.809863099999994,14.150356],[47.8622162,6.5175032]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u5!2m2!5m1!1sgcid_3german_1restaurant!1m4!1u5!2m2!5m1!1sgcid_3indian_1restaurant!1m4!1u2!2m2!2m1!1e1!1m4!1u1!2m2!1m1!1e1!1m4!1u1!2m2!1m1!1e2!2m1!1e2!2m1!1e5!2m1!1e1!2m1!1e3!3sIAEqAkRF,lf:1,lf_ui:9",
-        6: "Paste url in url-field of scrape-Page route",
-        7: "Click on Execute to scrape data",
-        8: "The scraping would take some time before it completes so as to avoid Googles suspicion and captcha test",
-        9: "Append /docs to this page's url to continue"
-    }
-
-@app.get("/data-via-page-url")
-async def scrape_page(url: str):
-    if not url:
-        raise HTTPException(status_code=404, detail="Input a url to scrape data")
-    try:
-        data = scrapePage(url)
-
-    except WebDriverException:
-        return {"Connection Error": "Please ensure your internet connection is secure and try again"}
-    
-    except Exception as e:
-        print(e)
-        return e
-    return data
 
 @app.get("/download")
 async def download_data():
