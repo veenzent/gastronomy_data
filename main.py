2721ab31453ea1b7ac4c2d02dcf8b2eee58c4b99
ODUME VINCENT TUOYO <odumevincent19@gmail.com>
2024-01-14 01:53:00 +0000

    restructured code

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from selenium.common.exceptions import NoSuchElementException, WebDriverExceptio
 app = FastAPI(title="G-Maps Gastronomy Data")
 
 chrome_options = Options()
-# chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 # chrome_options.add_argument("--disable-blink-features=AutomationControlled")
