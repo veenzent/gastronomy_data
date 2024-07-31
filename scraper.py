adbab52d12eec411e63e2c10751618f2cb5ea16b
veenzent <odumevincent19@gmail.com>
2024-07-31 16:58:58 +0100

    Setting up streamlit

--- scraper.py
+++ scraper.py
@@ -24,7 +24,9 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 
 
 class RestaurantScraper:
-    chromeDriver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    # service = ChromeService(ChromeDriverManager().install())
+    service = ChromeService(executable_path="./chromedriver.exe")
+    chromeDriver = webdriver.Chrome(service=service, options=chrome_options)
     wait = WebDriverWait(chromeDriver, 10)
 
     def __init__(self) -> None:
@@ -181,7 +183,7 @@ class Extractor(RestaurantScraper):
         sleep(3)
 
 
-# extractor = Extractor()
+extractor = Extractor()
 
 # # mainz = "https://www.google.com/search?client=firefox-b-d&sca_esv=596374102&tbs=lf:1,lf_ui:9&tbm=lcl&sxsrf=ACQVn0-2G5sPef-vv6QYCfU0u1cJpeTLYw:1704641224806&q=mainz+finthen+restaurant&rflfq=1&num=10&sa=X&ved=2ahUKEwil77G1y8uDAxUvaUEAHT3CDdgQjGp6BAgSEAE&biw=1525&bih=760&dpr=0.9#rlfi=hd:;si:;mv:[[49.997862299999994,8.1819598],[49.9712104,8.1506037]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u5!2m2!5m1!1sgcid_3pizza_1restaurant!1m4!1u5!2m2!5m1!1sgcid_3german_1restaurant!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e5!2m1!1e3!3sIAEqAkRF,lf:1,lf_ui:9"
 # urll = "https://www.google.com/search?client=firefox-b-d&sca_esv=594603375&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u5!2m2!5m1!1sgcid_3german_1restaurant!1m4!1u5!2m2!5m1!1sgcid_3indian_1restaurant!1m4!1u2!2m2!2m1!1e1!1m4!1u1!2m2!1m1!1e1!1m4!1u1!2m2!1m1!1e2!2m1!1e2!2m1!1e5!2m1!1e1!2m1!1e3!3sIAEqAkRF,lf:1,lf_ui:9&tbm=lcl&sxsrf=AM9HkKnqdXFj_FVNzgEjVYXigqgBUOtXnw:1703948168668&q=restaurants%20in%20germany&rflfq=1&num=10&sa=X&ved=2ahUKEwinmbzKtbeDAxWfS0EAHQLRDoEQjGp6BAgXEAE&biw=1525&bih=760&dpr=0.9&rlst=f#rlfi=hd:;si:;mv:[[52.809863099999994,14.150356],[47.8622162,6.5175032]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u5!2m2!5m1!1sgcid_3german_1restaurant!1m4!1u5!2m2!5m1!1sgcid_3indian_1restaurant!1m4!1u2!2m2!2m1!1e1!1m4!1u1!2m2!1m1!1e1!1m4!1u1!2m2!1m1!1e2!2m1!1e2!2m1!1e5!2m1!1e1!2m1!1e3!3sIAEqAkRF,lf:1,lf_ui:9"
---
+++ streamlit_client.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+from scraper import Extractor
+
+
+st.markdown('<h1 style="color: lightblue; text-align: center;">GASTRONOMY DATA</h1>', unsafe_allow_html=True)
+
+st.markdown("""
+    <h2>Steps to scrape gastronomy data from Google maps</h2>
+        <ol>
+            <li>Query your browser manually for city you want gastronomy data on e.g restaurants in Germany</li>
+            <li>Click on 'more places' to load list of restaurants in google maps</li>
+            <li>Copy and paste url of page from step 2 in the field below.</li>
+            <li>
+                Sampled_url = 
+                <a href="https://www.google.com/search?client=firefox-b-d&sca_esv=594603375&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u5!2m2!5m1!1sgcid_3german_1restaurant!1m4!1u5!2m2!5m1!1sgcid_3indian_1restaurant!1m4!1u2!2m2!2m1!1e1!1m4!1u1!2m2!1m1!1e1!1m4!1u1!2m2!1m1!1e2!2m1!1e2!2m1!1e5!2m1!1e1!2m1!1e3!3sIAEqAkRF,lf:1,lf_ui:9&tbm=lcl&sxsrf=AM9HkKnqdXFj_FVNzgEjVYXigqgBUOtXnw:1703948168668&q=restaurants%20in%20germany&rflfq=1&num=10&sa=X&ved=2ahUKEwinmbzKtbeDAxWfS0EAHQLRDoEQjGp6BAgXEAE&biw=1525&bih=760&dpr=0.9&rlst=f#rlfi=hd:;si:;mv:[[52.809863099999994,14.150356],[47.8622162,6.5175032]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u5!2m2!5m1!1sgcid_3german_1restaurant!1m4!1u5!2m2!5m1!1sgcid_3indian_1restaurant!1m4!1u2!2m2!2m1!1e1!1m4!1u1!2m2!1m1!1e1!1m4!1u1!2m2!1m1!1e2!2m1!1e2!2m1!1e5!2m1!1e1!2m1!1e3!3sIAEqAkRF,lf:1,lf_ui:9" class="sample" target="_blank">Restaurants in Germany</a>
+            </li>
+            <li>Click on the Scrape to start scraping data</li>
+            <li>The scraping would take some time before it completes. This is an explicit action to avoid Googles suspicion and captcha test.</li>
+        </ol>
+    """, unsafe_allow_html=True
+)
+
+
+# create an instace of Exctractor
+extractor = Extractor()
+
+def scrapeData(url: str):
+    # create a dataframe
+    df_data = {
+            "S/N": [],
+            "Name": [],
+            "Address": [],
+            "Nationality": [],
+            "Telephone Number": [],
+            "Website": [],
+            "Instagram Link": [],
+            "Facebook Link": [],
+            "Service Options": []
+        }
+    df = pd.DataFrame(df_data)
+
+    # load url
+    extractor.load_url_page(url)
+
+    index = 1
+    while extractor.click_restaurant():
+        data = {
+            "S/N": index,
+            "Name": extractor.extract_restaurant_name(),
+            "Address": extractor.extract_restaurant_address(),
+            "Nationality": extractor.extract_restaurant_nationality(),
+            "Telephone Number": extractor.extract_restaurant_telephone_number(),
+            "Website": extractor.extract_restaurant_website(),
+            "Instagram Link": extractor.extract_restaurant_instagram_link(),
+            "Facebook Link": extractor.extract_restaurant_facebook_link(),
+            "Service Options": extractor.extract_restaurant_service_options()
+        }
+
+        new_row = pd.Series(data)
+        df.loc[len(df)] = new_row
+        print(data, '\n')
+        index += 1
+        extractor.close_current_page()
+
+        # display dataframe
+        st.dataframe(df)
+
+        # limit scraping to 10 results(10 restaurants)
+        if index == 11:
+            break
+
+    extractor.quit_browser()
+
+
+# create a form
+form = st.form(key="my_form")
+url = form.text_input("Enter URL")
+form.form_submit_button("Scrape", on_click=scrapeData, kwargs=url:url)
